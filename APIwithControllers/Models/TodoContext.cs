ccc59ff8db6d60b0608d905337e9e73f37105e0e
alexOpb <strongbigwolf5@gmail.com>
2022-09-04 19:27:56 +1000

    Add the TodoContext database context

---
+++ APIwithControllers/Models/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIwithControllers.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) 
+            : base(options)
+        {
+            
+        }
+        
+        //null!
+        //By using the null-forgiving operator,
+        //you inform the compiler that passing null is expected and shouldn't be warned about.
+        public DbSet<TodoItem> To { get; set; } = null!;
+    }
+}
